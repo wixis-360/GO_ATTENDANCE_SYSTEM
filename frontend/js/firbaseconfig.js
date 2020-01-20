67e56a07861ccfe4604c63eb218992fc80f602b6
sachinlakshitha <sachinlakshitha@bitbucket.org>
2020-01-20 13:22:40 +0530

    frontend module added

---
+++ frontend/js/firbaseconfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBSNGJXaqm_Cp7Jdn5VR6v1N6KvxH6U53A",
+    authDomain: "attendance-ce593.firebaseapp.com",
+    databaseURL: "https://attendance-ce593.firebaseio.com",
+    projectId: "attendance-ce593",
+    storageBucket: "attendance-ce593.appspot.com",
+    messagingSenderId: "274580348053",
+    appId: "1:274580348053:web:096191116cd18632760f9c",
+    measurementId: "G-0K7ZQVMER6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
