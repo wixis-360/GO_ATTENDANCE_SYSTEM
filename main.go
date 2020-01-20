39943ae340ba9b7559bf1eff4f3399231f7d56f7
sachinlakshitha <sachinlakshitha@bitbucket.org>
2020-01-20 12:13:50 +0530

    Add initial project

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	_ "attendance/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "POST", "DELETE", "GET"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	beego.Run()
+}
